31b91251af99863784a81ec1dd9f107ad64d6c05
VergilH <34698426+VergilH@users.noreply.github.com>
2020-05-31 18:22:31 +0800

    search.vue change

--- music-paly/src/components/search.vue
+++ music-paly/src/components/search.vue
@@ -1,5 +1,10 @@
 <template>
-<div>Search</div>
+<div>
+  <div id="search-btn">这是搜索按钮</div>
+  <span class="hot-key" v-for="key in hotKey" :key="key.w">
+    {{key.k}}
+  </span>
+</div>
 </template>
 
 <script>
@@ -8,15 +13,20 @@ export default {
   name: 'search',
   data () {
     return {
+      hotKey: []
     }
   },
   methods: {
-    getSearchHotKey () {
-      getHotKey()
+    getSearchHotKey: function () { // 热门搜索
+      getHotKey().then((res) => { // 获取外部js返回数据
+        this.hotKey = res.data.data.hotkey
+        console.log(this.hotKey)
+      })
     },
-    getSearch () {
+    getSearch () { // 搜索关键词
       search()
     }
+    /* this.hotKey = res.data.data */
   },
   created () {
     this.getSearch()
@@ -25,5 +35,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#search-btn {
+  margin: 20px auto;
+  width: 100%;
+}
+.hot-key {
+  float: left;
+  display: inline-block;
+  margin: 5px;
+  padding: 5px 10px;
+  border: 1px solid #000000;
+  border-radius: 4px;
+  line-height: 16px;
+  box-sizing: border-box;
+}
 </style>
